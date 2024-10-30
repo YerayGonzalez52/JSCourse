babe0f745bce3f7a61b1c9bbfb37a5c0398981be
Yeray <yeraygonzalez52@gmail.com>
2024-10-30 11:04:30 -0400

    repaso

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -1,45 +1,43 @@
 let books = [];
 
 function addBook(){
-    const bookName = document.getElementById('bookName').value;
-    const authorName = document.getElementById('authorName').value;
-    const bookDescription = document.getElementById('bookDescription').value;
-    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+    const bookName = document.getElementById("bookName").value;
+    const authorName  = document.getElementById("authorName").value;
+    const bookDescription  = document.getElementById("bookDescription").value;
+    const pagesNumber  = document.getElementById("pagesNumber").value;
+
     if(bookName && authorName && bookDescription && !isNaN(pagesNumber)){
         const book = {
             name: bookName,
-            authorName : authorName,
-            bookDescription : bookDescription,
-            pagesNumber : pagesNumber
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
         };
         books.push(book);
         showbooks();
         clearInputs();
     }
     else{
-        alert("Fill all the fields")
+        alert('Please fill in all fields correctly.');
     }
 }
 
-function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-        <p><strong>Book Name: </strong>${book.name}</p>
-        <p><strong>Author Name:</strong> ${book.authorName}</p>
-        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="deletebook(${index})">Delete Book</button>`
-    );
-    document.getElementById('books').innerHTML = booksDiv.join('');
+function showbooks(){
+    let booksDiv = "";
+    for(i = 0; i < books.length; i++){
+        booksDiv = booksDiv + `<h1>book Number: ${i + 1}</h1>
+        <p><strong>Book Name: </strong>${books[i].name}</p>
+        <p><strong>Author Name:</strong> ${books[i].authorName}</p>
+        <p><strong>Book Description:</strong> ${books[i].bookDescription}</p>
+        <p><strong>No. of Pages:</strong> ${books[i].pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${i})">Delete Book</button>`
+    }
+    document.getElementById('books').innerHTML = booksDiv;
 }
 
- function clearInputs() {
-            document.getElementById('bookName').value = '';
-            document.getElementById('authorName').value = '';
-            document.getElementById('bookDescription').value = '';
-            document.getElementById('pagesNumber').value = '';
- }
-
- function deletebook(index){
-    books.splice(index, 1); // Elimina el libro en la posición "index"
-    showbooks(); // Actualiza la lista de libros después de eliminar uno
- }
+function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
